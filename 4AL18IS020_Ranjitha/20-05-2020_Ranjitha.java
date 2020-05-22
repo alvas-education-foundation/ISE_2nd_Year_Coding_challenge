5df5a11d3b7ae510cd8615e17376c666eff8859b
Ranjitha111 <65382384+Ranjitha111@users.noreply.github.com>
2020-05-22 17:46:34 +0530

    Update and rename 20-05-2020_Ranjitha.txt to 20-05-2020_Ranjitha.java

--- 4AL18IS020_Ranjitha/20-05-2020_Ranjitha.java
+++ 4AL18IS020_Ranjitha/20-05-2020_Ranjitha.java
@@ -1,10 +1,10 @@
-# ISE_2nd_Year_Coding_challenge
+/*# ISE_2nd_Year_Coding_challenge
 
 Question:
-In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below. To make the problem a little easier, a portion of the code is provided for you in the editor.
+In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below. To make the problem a little easier, a portion of the code is provided for you in the editor.*/
 
 
-Ans:
+//Ans:
 public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
