6a5ea33728129cebdb7f72147824d8be15430516
Abhiman <abhi656.aa@gmail.com>
2020-05-26 19:22:59 +0530

    change1

---
+++ 4AL18IS002__AbhimanH.R/pradeepsir_coding_challenge/challenge_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ 
+int main()
+{
+      int num1, num2, a, b=4, count = 0 ;
+      printf("\nEnter First Number:\t");
+      scanf("%d", &a);
+      num1 = a;
+      num2 = b;
+      while(num1 >= num2)
+      {
+            num1 = num1 - num2;
+            count++;
+      }
+      printf("\nDivision of %d and %d:\nQuotient:\t%d\nRemainder:\t%d\n", a, b, count, num1);
+      return 0;
+}
