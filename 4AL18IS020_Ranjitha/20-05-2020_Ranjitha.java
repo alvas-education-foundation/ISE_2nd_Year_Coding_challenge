ac6b26db8cfabbea5d814f1e99f4be53deaa6da6
Ranjitha111 <ranjithakuledu20@gmail.com>
2020-05-27 12:18:53 +0530

    coding

--- 4AL18IS020_Ranjitha/20-05-2020_Ranjitha.java
+++ 4AL18IS020_Ranjitha/20-05-2020_Ranjitha.java
@@ -1,10 +1,11 @@
-# ISE_2nd_Year_Coding_challenge
+//# ISE_2nd_Year_Coding_challenge
 
-Question:
-In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below. To make the problem a little easier, a portion of the code is provided for you in the editor.
+//Question:
+//In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below. To make the problem a little easier, a portion of the code is provided for you in the editor.
 
 
-Ans:
+//Ans:
+public class solution{
 public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
@@ -18,3 +19,4 @@ public static void main(String[] args) {
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
     }
+}
